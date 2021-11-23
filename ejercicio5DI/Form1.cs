86b6ee54ab56da8388ad9fcc35a35b50f07b36fb
Oscar fariña <61348449+ofaricamp@users.noreply.github.com>
2021-11-23 20:04:50 +0100

    Add files via upload

---
+++ ejercicio5DI/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ejercicio5DI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void addbtn_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(textBox1.Text);
+        }
+
+        private void deletebtn_Click(object sender, EventArgs e)
+        {
+            if (listBox1.Items.Count > 0)
+            {
+                for (int i = listBox1.SelectedItems.Count-1; i >= 0 ; i--)
+                {
+                    listBox1.Items.Remove(listBox1.SelectedItems[i]);
+                }
+            }
+            if (listBox2.Items.Count > 0)
+            {
+                for (int i = listBox2.SelectedItems.Count - 1; i >= 0; i--)
+                {
+                    listBox2.Items.Remove(listBox2.SelectedItems[i]);
+                }
+            }
+
+        }
+
+        private void transferbtn1_Click(object sender, EventArgs e)
+        {
+            if (listBox1.Items.Count > 0)
+            {
+                for (int i = 0; i < listBox1.SelectedItems.Count; i++)
+                {
+                    listBox2.Items.Insert(i, listBox1.SelectedItems[i]);
+                }
+
+                for (int i = 0; i < listBox1.SelectedItems.Count; i++)
+                {
+                    listBox1.Items.Remove(listBox1.SelectedItems[i]);
+                }
+            }
+        }
+
+        private void transferbtn2_Click(object sender, EventArgs e)
+        {
+            if (listBox2.Items.Count > 0)
+            {
+                listBox1.Items.Insert(0,listBox2.SelectedItem);
+                listBox2.Items.Remove(listBox2.SelectedItem);
+            }
+        }
+    }
+}
