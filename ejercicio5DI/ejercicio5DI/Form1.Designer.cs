3638e922e9e0acdee8b8d23b3b529f2e256aa1a9
Oscar fariña <61348449+ofaricamp@users.noreply.github.com>
2021-11-23 20:09:06 +0100

    Add files via upload

---
+++ ejercicio5DI/ejercicio5DI/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace ejercicio5DI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.addbtn = new System.Windows.Forms.Button();
+            this.deletebtn = new System.Windows.Forms.Button();
+            this.transferbtn1 = new System.Windows.Forms.Button();
+            this.transferbtn2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(50, 46);
+            this.listBox1.MultiColumn = true;
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.listBox1.Size = new System.Drawing.Size(106, 173);
+            this.listBox1.TabIndex = 0;
+            // 
+            // addbtn
+            // 
+            this.addbtn.Location = new System.Drawing.Point(342, 71);
+            this.addbtn.Name = "addbtn";
+            this.addbtn.Size = new System.Drawing.Size(75, 23);
+            this.addbtn.TabIndex = 2;
+            this.addbtn.Text = "ADD";
+            this.addbtn.UseVisualStyleBackColor = true;
+            this.addbtn.Click += new System.EventHandler(this.addbtn_Click);
+            // 
+            // deletebtn
+            // 
+            this.deletebtn.Location = new System.Drawing.Point(342, 100);
+            this.deletebtn.Name = "deletebtn";
+            this.deletebtn.Size = new System.Drawing.Size(75, 23);
+            this.deletebtn.TabIndex = 3;
+            this.deletebtn.Text = "DELETE";
+            this.deletebtn.UseVisualStyleBackColor = true;
+            this.deletebtn.Click += new System.EventHandler(this.deletebtn_Click);
+            // 
+            // transferbtn1
+            // 
+            this.transferbtn1.Location = new System.Drawing.Point(342, 129);
+            this.transferbtn1.Name = "transferbtn1";
+            this.transferbtn1.Size = new System.Drawing.Size(75, 23);
+            this.transferbtn1.TabIndex = 4;
+            this.transferbtn1.Text = "TRANSFER";
+            this.transferbtn1.UseVisualStyleBackColor = true;
+            this.transferbtn1.Click += new System.EventHandler(this.transferbtn1_Click);
+            // 
+            // transferbtn2
+            // 
+            this.transferbtn2.Location = new System.Drawing.Point(342, 159);
+            this.transferbtn2.Name = "transferbtn2";
+            this.transferbtn2.Size = new System.Drawing.Size(75, 23);
+            this.transferbtn2.TabIndex = 5;
+            this.transferbtn2.Text = "TRANSFER2";
+            this.transferbtn2.UseVisualStyleBackColor = true;
+            this.transferbtn2.Click += new System.EventHandler(this.transferbtn2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(121, 231);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(252, 234);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(62, 234);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "label2";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(178, 46);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(109, 173);
+            this.listBox2.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.addbtn;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.transferbtn2);
+            this.Controls.Add(this.transferbtn1);
+            this.Controls.Add(this.deletebtn);
+            this.Controls.Add(this.addbtn);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button addbtn;
+        private System.Windows.Forms.Button deletebtn;
+        private System.Windows.Forms.Button transferbtn1;
+        private System.Windows.Forms.Button transferbtn2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
