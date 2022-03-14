d35e75916293e36323fe688ba3433337824bd2e3
Oscar fariña <61348449+ofaricamp@users.noreply.github.com>
2022-03-14 10:34:04 +0100

    Add files via upload

---
+++ Bol5_Ejer3/Bol5_Ejer3/ReproductorSimple.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace Bol5_Ejer3
+{
+    partial class ReproductorSimple
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Playbtn = new System.Windows.Forms.Button();
+            this.Time = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Playbtn
+            // 
+            this.Playbtn.Location = new System.Drawing.Point(49, 184);
+            this.Playbtn.Name = "Playbtn";
+            this.Playbtn.Size = new System.Drawing.Size(75, 23);
+            this.Playbtn.TabIndex = 0;
+            this.Playbtn.Text = "Play/Pause";
+            this.Playbtn.UseVisualStyleBackColor = true;
+            this.Playbtn.Click += new System.EventHandler(this.Playbtn_Click);
+            // 
+            // Time
+            // 
+            this.Time.AutoSize = true;
+            this.Time.Location = new System.Drawing.Point(46, 210);
+            this.Time.Name = "Time";
+            this.Time.Size = new System.Drawing.Size(61, 13);
+            this.Time.TabIndex = 1;
+            this.Time.Text = "HH:MM:SS";
+            // 
+            // ReproductorSimple
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.Time);
+            this.Controls.Add(this.Playbtn);
+            this.Name = "ReproductorSimple";
+            this.Size = new System.Drawing.Size(211, 233);
+            this.Load += new System.EventHandler(this.ReproductorSimple_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Playbtn;
+        private System.Windows.Forms.Label Time;
+    }
+}
---
+++ Bol5_Ejer3/Bol5_Ejer3/ReproductorSimple.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bol5_Ejer3
+{
+    public enum ePlayPause
+    {
+        Play,
+        Pause
+    }
+    public partial class ReproductorSimple : UserControl
+    {
+        private ePlayPause playPause;
+        [Category("Appearance")]
+        [Description("Indica el diseño que aparece")]
+        public ePlayPause PlayPause
+        {
+            set
+            {
+                playPause = value;
+                this.Refresh();
+            }
+            get
+            {
+                return PlayPause;
+            }
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Graphics g = e.Graphics;
+            int grosor = 0;
+            int offsetX = 0;
+            int offsetY = 0;
+            int altura = this.Height;
+
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            SolidBrush b = new SolidBrush(this.ForeColor);
+
+
+
+            switch (PlayPause)
+            {
+                case ePlayPause.Play:
+                    e.Graphics.FillRectangle(b, 0, 0, this.Width, this.Height);
+                    grosor = 35;
+                    g.DrawEllipse(new Pen(Color.Black, grosor), grosor, grosor, altura, altura);
+                    grosor = 4;
+                    Pen lapiz = new Pen(Color.White, grosor);
+                    g.DrawLine(lapiz,grosor,grosor,altura,altura);
+                    g.DrawLine(lapiz, altura, grosor, grosor, altura);
+                    g.DrawLine(lapiz, altura, altura, grosor, grosor);
+                    offsetX = altura + grosor;
+                    offsetY = grosor;
+                    lapiz.Dispose();
+                    break;
+                case ePlayPause.Pause:
+                    e.Graphics.FillRectangle(b, 0, 0, this.Width, this.Height);
+                    grosor = 35;
+                    g.DrawEllipse(new Pen(Color.Black, grosor), grosor,grosor,altura,altura);
+                    grosor = 4;
+                    g.DrawLine(new Pen(Color.White, grosor), 0, altura, 0, altura);
+                    g.DrawLine(new Pen(Color.White, grosor), grosor, altura, grosor, altura);
+                    offsetX = altura + grosor;
+                    offsetY = grosor;
+                    break;
+                default:
+                    break;
+            }
+            b.Dispose();
+        }
+
+
+
+        protected override void OnMouseClick(MouseEventArgs e)
+        {
+            base.OnMouseClick(e);
+            if (e.X <= this.Width && e.Y <= this.Height)
+            {
+                Reproduce(this, new EventArgs());
+            }
+        }
+
+
+
+
+        [Category("La propiedad Onclick del componente")]
+        [Description("Se lanza cuando se hace click en el componente")]
+        public event System.EventHandler Reproduce;
+       
+        
+        
+        
+        
+        
+        public ReproductorSimple()
+        {
+            InitializeComponent();
+        }
+
+        private void ReproductorSimple_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Playbtn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
