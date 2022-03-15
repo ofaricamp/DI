093ad031c3787ef84a4a05b52ce89db239ca51d7
Oscar fariña <61348449+ofaricamp@users.noreply.github.com>
2022-03-15 10:48:13 +0100

    Add files via upload

---
+++ RespasoExamenRecuDI/Practica/RespasoExamenRecuDI/LabelTextBox.Designer.cs
@@ -0,0 +1,80 @@
+﻿
+namespace RespasoExamenRecuDI
+{
+    partial class LabelTextBox
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lbl = new System.Windows.Forms.Label();
+            this.txt = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.SuspendLayout();
+            // 
+            // lbl
+            // 
+            this.lbl.AutoSize = true;
+            this.lbl.Location = new System.Drawing.Point(4, 56);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(35, 13);
+            this.lbl.TabIndex = 0;
+            this.lbl.Text = "label1";
+            // 
+            // txt
+            // 
+            this.txt.Location = new System.Drawing.Point(22, 95);
+            this.txt.Name = "txt";
+            this.txt.Size = new System.Drawing.Size(100, 20);
+            this.txt.TabIndex = 1;
+            this.txt.TextChanged += new System.EventHandler(this.txt_TextChanged);
+            this.txt.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txt_KeyUp);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // LabelTextBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.txt);
+            this.Controls.Add(this.lbl);
+            this.Name = "LabelTextBox";
+            this.Load += new System.EventHandler(this.LabelTextBox_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl;
+        private System.Windows.Forms.TextBox txt;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
---
+++ RespasoExamenRecuDI/Practica/RespasoExamenRecuDI/LabelTextBox.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RespasoExamenRecuDI
+{
+    public enum ePosicion
+    {
+        IZQUIERDA,DERECHA
+    }
+
+    public partial class LabelTextBox : UserControl
+    {
+        public LabelTextBox()
+        {
+            InitializeComponent();
+            TextLbl = "Funciono";
+            TextText = "";
+            recolocar();
+        }
+
+        private ePosicion posicion = ePosicion.IZQUIERDA;
+        
+        [Category("Appearence")]
+        [Description("Indica si la Label se situa a la IZQUIERDA o a la DERECHA del TextBox")]
+        public ePosicion Posicion
+        {
+            set 
+            {
+                if (Enum.IsDefined(typeof(ePosicion),value))
+                {
+                    posicion = value;
+                    recolocar();
+                    if (CambiaPosicion != null)
+                    {
+                        CambiaPosicion(this, new EventArgs());
+                    }
+                }
+                else
+                {
+                    throw new InvalidEnumArgumentException();
+                }
+            }
+            get { return posicion; }
+        }
+
+        private int separacion = 0;
+        [Category("Design")]
+        [Description("Pixeles de separacion entre la Label y el Textbox")]
+        public int Separacion
+        {
+            set
+            {
+                if (value >= 0)
+                {
+                    separacion = value;
+                    recolocar();
+                    if (CambiaSeparacion != null)
+                    {
+                        CambiaSeparacion(this, new EventArgs());
+                    }
+                   
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+            get { return separacion; }
+        }
+        
+        [Category("Appearence")]
+        [Description("Texto asociado a la Label del control")]
+        public string TextLbl
+        {
+            set 
+            {
+                lbl.Text = value;
+                recolocar();
+            }
+            get { return lbl.Text; }
+        }
+
+        [Category("Appearence")]
+        [Description("Texto asociado al TextBox del control")]
+        public string TextText
+        {
+            set
+            {
+                txt.Text = value;
+            }
+            get { return txt.Text; }
+        }
+
+        [Category("Appearence")]
+        [Description("Propiedad PaswordChar del TextBox")]
+        public char Pswchar
+        {
+            set
+            {
+                txt.PasswordChar = value;
+            }
+            get
+            {
+                return txt.PasswordChar;
+            }
+        }
+        void recolocar() 
+        {
+            switch (posicion)
+            {
+                case ePosicion.IZQUIERDA:
+                    lbl.Location = new Point(0, 0);
+                    txt.Location = new Point(lbl.Width + Separacion, 0);
+                    txt.Width = this.Width - lbl.Width - Separacion;
+                    this.Height = Math.Max(txt.Height, lbl.Height);
+                    break;
+
+                case ePosicion.DERECHA:
+                    txt.Location = new Point(0, 0);
+                    //txt.Width = this.Width - lbl.Width - Separacion;
+                    this.Width = txt.Width + lbl.Width + Separacion;
+                    lbl.Location = new Point(txt.Width + Separacion, 0);
+                    this.Height = Math.Max(txt.Height, lbl.Height);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void LabelTextBox_SizeChanged(object sender, EventArgs e)
+        {
+            recolocar();
+        }
+
+        protected override void OnSizeChanged(EventArgs e)
+        {
+            base.OnSizeChanged(e);
+            recolocar();
+        }
+        private void txt_KeyUp(object sender, KeyEventArgs e)
+        {
+            this.OnKeyUp(e);
+        }
+
+        private void LabelTextBox_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void labelTextBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            this.Text = "Letra: " + e.KeyChar;
+        }
+        private void txt_TextChanged(object sender, EventArgs e)
+        {
+            if (TxtChanged != null)
+            {
+                TxtChanged(this, new EventArgs());
+            }
+        }
+
+        [Category("La propiedad cambió")]
+        [Description("Se lanza cuando la propiedad Posicion cambia")]
+        public event System.EventHandler CambiaPosicion;
+
+        [Category("La propiedad cambio")]
+        [Description("Se lanza cuando la propiedad separacion cambia")]
+        public event System.EventHandler CambiaSeparacion;
+
+        [Category("La propiedad TextChanged cambio")]
+        [Description("Se lanza cuando ocurre un textChanged en el textbox")]
+        public event System.EventHandler TxtChanged;
+
+     
+    }
+}
