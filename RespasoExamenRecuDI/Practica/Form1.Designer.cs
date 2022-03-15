fc7fb2b85fea97d7d5c23d73212fbd81ab9c3b34
Oscar fariña <61348449+ofaricamp@users.noreply.github.com>
2022-03-15 10:44:03 +0100

    Add files via upload

---
+++ RespasoExamenRecuDI/Practica/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿
+namespace Practica
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.separbtn = new System.Windows.Forms.Button();
+            this.posbtn = new System.Windows.Forms.Button();
+            this.labelTextBox2 = new RespasoExamenRecuDI.LabelTextBox();
+            this.SuspendLayout();
+            // 
+            // separbtn
+            // 
+            this.separbtn.Location = new System.Drawing.Point(141, 233);
+            this.separbtn.Name = "separbtn";
+            this.separbtn.Size = new System.Drawing.Size(75, 23);
+            this.separbtn.TabIndex = 3;
+            this.separbtn.Text = "Separacion";
+            this.separbtn.UseVisualStyleBackColor = true;
+            this.separbtn.Click += new System.EventHandler(this.separbtn_Click_1);
+            // 
+            // posbtn
+            // 
+            this.posbtn.Location = new System.Drawing.Point(223, 232);
+            this.posbtn.Name = "posbtn";
+            this.posbtn.Size = new System.Drawing.Size(75, 23);
+            this.posbtn.TabIndex = 4;
+            this.posbtn.Text = "Posicion";
+            this.posbtn.UseVisualStyleBackColor = true;
+            this.posbtn.Click += new System.EventHandler(this.posbtn_Click);
+            // 
+            // labelTextBox2
+            // 
+            this.labelTextBox2.Location = new System.Drawing.Point(141, 207);
+            this.labelTextBox2.Name = "labelTextBox2";
+            this.labelTextBox2.Posicion = RespasoExamenRecuDI.ePosicion.IZQUIERDA;
+            this.labelTextBox2.Pswchar = '*';
+            this.labelTextBox2.Separacion = 0;
+            this.labelTextBox2.Size = new System.Drawing.Size(150, 20);
+            this.labelTextBox2.TabIndex = 1;
+            this.labelTextBox2.TextLbl = "Funciono";
+            this.labelTextBox2.TextText = "";
+            this.labelTextBox2.CambiaPosicion += new System.EventHandler(this.labelTextBox2_CambiaPosicion);
+            this.labelTextBox2.CambiaSeparacion += new System.EventHandler(this.labelTextBox2_CambiaSeparacion);
+            this.labelTextBox2.KeyUp += new System.Windows.Forms.KeyEventHandler(this.labelTextBox2_KeyUp);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.separbtn;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.posbtn);
+            this.Controls.Add(this.separbtn);
+            this.Controls.Add(this.labelTextBox2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private RespasoExamenRecuDI.LabelTextBox labelTextBox2;
+        private System.Windows.Forms.Button separbtn;
+        private System.Windows.Forms.Button posbtn;
+    }
+}
+
---
+++ RespasoExamenRecuDI/Practica/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            labelTextBox2.TextText = Convert.ToString(labelTextBox2.TextLbl);
+        }
+        private void separbtn_Click_1(object sender, EventArgs e)
+        {
+            labelTextBox2.Separacion++;
+        }
+
+        private void labelTextBox2_CambiaPosicion(object sender, EventArgs e)
+        {
+            this.Text = labelTextBox2.Posicion.ToString();
+        }
+
+        private void posbtn_Click(object sender, EventArgs e)
+        {
+            if (labelTextBox2.Posicion == RespasoExamenRecuDI.ePosicion.DERECHA)
+            {
+                labelTextBox2.Posicion = RespasoExamenRecuDI.ePosicion.IZQUIERDA;
+            }
+            else
+            {
+                labelTextBox2.Posicion = RespasoExamenRecuDI.ePosicion.DERECHA;
+            }
+        }
+
+        private void labelTextBox2_CambiaSeparacion(object sender, EventArgs e)
+        {
+            this.Text = labelTextBox2.Separacion.ToString();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Deseas Salir?","Seguro?",MessageBoxButtons.OKCancel,MessageBoxIcon.Question) ==DialogResult.Cancel )
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void labelTextBox2_KeyUp(object sender, KeyEventArgs e)
+        {
+            this.Text = e.KeyCode.ToString();
+        }
+    }
+}
