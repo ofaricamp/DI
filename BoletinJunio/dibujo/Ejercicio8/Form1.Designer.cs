843e44bc2fd453bd887b4c35d5371e750384c307
Oscar fariña <61348449+ofaricamp@users.noreply.github.com>
2022-05-12 12:13:21 +0200

    Add files via upload

---
+++ BoletinJunio/dibujo/Ejercicio8/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace Ejercicio8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.customControl14 = new dibujo.CustomControl1();
+            this.customControl13 = new dibujo.CustomControl1();
+            this.customControl12 = new dibujo.CustomControl1();
+            this.customControl11 = new dibujo.CustomControl1();
+            this.SuspendLayout();
+            // 
+            // customControl14
+            // 
+            this.customControl14.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.customControl14.Gradiente = false;
+            this.customControl14.Imagen = global::Ejercicio8.Properties.Resources.índice;
+            this.customControl14.Location = new System.Drawing.Point(12, 318);
+            this.customControl14.Marca = dibujo.CustomControl1.eMarca.IMAGEN;
+            this.customControl14.Name = "customControl14";
+            this.customControl14.PrimerColor = System.Drawing.Color.Orange;
+            this.customControl14.SegundoColor = System.Drawing.Color.Blue;
+            this.customControl14.Size = new System.Drawing.Size(632, 120);
+            this.customControl14.TabIndex = 3;
+            this.customControl14.Text = "customControl14";
+            this.customControl14.ClickEnMarca += new System.EventHandler(this.customControl11_ClickEnMarca);
+            // 
+            // customControl13
+            // 
+            this.customControl13.Gradiente = true;
+            this.customControl13.Imagen = null;
+            this.customControl13.Location = new System.Drawing.Point(12, 218);
+            this.customControl13.Marca = dibujo.CustomControl1.eMarca.CIRCULO;
+            this.customControl13.Name = "customControl13";
+            this.customControl13.PrimerColor = System.Drawing.Color.Maroon;
+            this.customControl13.SegundoColor = System.Drawing.Color.Chartreuse;
+            this.customControl13.Size = new System.Drawing.Size(152, 63);
+            this.customControl13.TabIndex = 2;
+            this.customControl13.Text = "customControl13";
+            this.customControl13.ClickEnMarca += new System.EventHandler(this.customControl11_ClickEnMarca);
+            // 
+            // customControl12
+            // 
+            this.customControl12.Gradiente = false;
+            this.customControl12.Imagen = null;
+            this.customControl12.Location = new System.Drawing.Point(12, 175);
+            this.customControl12.Marca = dibujo.CustomControl1.eMarca.CRUZ;
+            this.customControl12.Name = "customControl12";
+            this.customControl12.PrimerColor = System.Drawing.Color.Orange;
+            this.customControl12.SegundoColor = System.Drawing.Color.Blue;
+            this.customControl12.Size = new System.Drawing.Size(108, 15);
+            this.customControl12.TabIndex = 1;
+            this.customControl12.Text = "customControl12";
+            this.customControl12.ClickEnMarca += new System.EventHandler(this.customControl11_ClickEnMarca);
+            // 
+            // customControl11
+            // 
+            this.customControl11.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.customControl11.Gradiente = true;
+            this.customControl11.Imagen = null;
+            this.customControl11.Location = new System.Drawing.Point(12, 12);
+            this.customControl11.Marca = dibujo.CustomControl1.eMarca.CIRCULO;
+            this.customControl11.Name = "customControl11";
+            this.customControl11.PrimerColor = System.Drawing.Color.Orange;
+            this.customControl11.SegundoColor = System.Drawing.Color.Blue;
+            this.customControl11.Size = new System.Drawing.Size(733, 130);
+            this.customControl11.TabIndex = 0;
+            this.customControl11.Text = "Bibibi Babibi BUUU";
+            this.customControl11.ClickEnMarca += new System.EventHandler(this.customControl11_ClickEnMarca);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.customControl14);
+            this.Controls.Add(this.customControl13);
+            this.Controls.Add(this.customControl12);
+            this.Controls.Add(this.customControl11);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Ejercicio 7";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private dibujo.CustomControl1 customControl11;
+        private dibujo.CustomControl1 customControl12;
+        private dibujo.CustomControl1 customControl13;
+        private dibujo.CustomControl1 customControl14;
+    }
+}
+
---
+++ BoletinJunio/dibujo/dibujo/CustomControl1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dibujo
+{
+    public partial class CustomControl1 : Control
+    {
+        public enum eMarca
+        {
+            CRUZ,
+            CIRCULO,
+            IMAGEN,
+            NADA
+        }
+
+        Rectangle rectangle;
+        
+        public CustomControl1()
+        {
+
+            InitializeComponent();
+            primerColor = Color.Orange;
+            segundoColor = Color.Blue;
+            gradiente = false;
+        }
+
+        private eMarca marca = eMarca.NADA;
+
+        [Category("Appearance")]
+        [Description("Indica qué marca aparecerá junto al texto")]
+        public eMarca Marca
+        {
+            set 
+            {
+                marca = value;
+                Refresh();
+            }
+            get { return marca; }
+        }
+
+        private Image imagen;
+
+        [Category("Appearance")]
+        [Description("Imagen que se pondra como marca en la etiqueta")]
+        public Image Imagen
+        {
+            set {
+                imagen = value;
+
+                if (imagen == null)
+                {
+                    marca = eMarca.NADA;
+                }
+
+                Refresh();
+            }
+            get
+            {
+                return imagen;
+            }
+        }
+
+        private Color primerColor;
+
+        [Category("Appearance")]
+        [Description("Es el primer color a elegir para el fondo gradiente")]
+        public Color PrimerColor
+        {
+            set 
+            {
+                primerColor = value;
+                Refresh();
+            }
+            get { return primerColor; }
+        }
+
+        private Color segundoColor;
+
+        [Category("Appearance")]
+        [Description("Es el segundo color a elegir para el fondo gradiente")]
+        public Color SegundoColor
+        {
+            set 
+            {
+                segundoColor = value;
+                Refresh();
+            }
+            get { return segundoColor; }
+        }
+
+        private bool gradiente;
+        
+        [Category("Appearance")]
+        [Description("Determina si se desea que la etiqueta disponga de un fondo gradiente o no")]
+        public bool Gradiente
+        {
+            set
+            { 
+                gradiente = value;
+                Refresh();
+            }
+            get { return gradiente; }
+        }
+
+        
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+
+            Graphics g = pe.Graphics;
+            int grosor = 0;
+            int offsetX = 0;
+            int offsetY = 0;
+            int height = Font.Height;
+
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+
+            if (gradiente)
+            {
+                LinearGradientBrush linearGradient = new LinearGradientBrush(new Point(0, 0), new Point(150, 150),
+                    PrimerColor, SegundoColor);
+                g.FillRectangle(linearGradient, 0, 0, Width, Height);
+                linearGradient.Dispose();
+            }
+
+            switch (Marca)
+            {
+                case eMarca.CRUZ:
+                    grosor = 3;
+                    g.DrawLine(new Pen(Color.Red, grosor), grosor, grosor, height, height);
+                    g.DrawLine(new Pen(Color.Red, grosor), height, grosor, grosor, height);
+                    offsetX = height + grosor;
+                    offsetY = grosor / 2;
+                    rectangle = new Rectangle(grosor, grosor, height, height);
+                    g.DrawRectangle(new Pen(Color.Transparent), rectangle);
+                    break;
+                case eMarca.CIRCULO:
+                    grosor = 25;
+                    g.DrawEllipse(new Pen(Color.MediumVioletRed, grosor), grosor, grosor, height, height);
+                    offsetX = height + grosor;
+                    offsetY = grosor;
+
+                    rectangle = new Rectangle(grosor, grosor, height, height);
+                    g.DrawRectangle(new Pen(Color.Transparent), rectangle);
+                    break;
+                case eMarca.IMAGEN:
+                    if (Imagen != null)
+                    {
+                       
+                        grosor = 20;
+                        g.DrawImage(Imagen, grosor, grosor, height, height);
+                        offsetX = height + grosor;
+                        offsetY = grosor;
+                    }
+
+                    rectangle = new Rectangle(grosor, grosor, height, height);
+                    g.DrawRectangle(new Pen(Color.Transparent), rectangle);
+                    break;
+                case eMarca.NADA:
+                    break;
+            }
+
+            SolidBrush bruh = new SolidBrush(ForeColor);
+            g.DrawString(this.Text, Font, bruh, offsetX + grosor, offsetY);
+            Size size = g.MeasureString(this.Text, this.Font).ToSize();
+            this.Size = new Size(size.Width + offsetX + grosor, size.Height + offsetY * 2);
+            bruh.Dispose();
+        }
+
+        [Category("Ejercicio")]
+        [Description("Se ejecuta si se pulsa la marca de la etuiqueta")]
+        public event EventHandler ClickEnMarca;
+
+        protected override void OnMouseClick(MouseEventArgs e)
+        {
+            base.OnMouseClick(e);
+            if (e.X <= rectangle.Right)
+            {
+                if (ClickEnMarca != null)
+                {
+                    ClickEnMarca(this, new EventArgs());
+                }
+            }
+        }
+
+    }
+
+}
