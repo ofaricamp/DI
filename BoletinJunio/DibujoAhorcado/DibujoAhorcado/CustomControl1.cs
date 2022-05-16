0ef9308875029ebd1b8c99b1f5bf0ef17855376d
Oscar fariña <61348449+ofaricamp@users.noreply.github.com>
2022-05-16 13:55:31 +0200

    Add files via upload

---
+++ BoletinJunio/DibujoAhorcado/DibujoAhorcado/CustomControl1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DibujoAhorcado
+{
+    public delegate void DelegadoAhorcado(Graphics g,Pen pen, int x1, int y1, int x2, int y2,bool ellipse);
+
+    public partial class CustomControl1 : Control
+    {
+        static void Pintado(Graphics g,Pen pen, int x1,int y1, int x2,int y2, bool ellipse)
+        {
+            if (!ellipse)
+            {
+                g.DrawLine(pen, x1, y1, x2, y2);
+            }
+            else
+            {
+                g.DrawEllipse(pen, x1, y1, x2, y2);
+            }
+        }
+        public DelegadoAhorcado pintarDelegado = new DelegadoAhorcado(Pintado);
+        public DelegadoAhorcado[] delegados = new DelegadoAhorcado[] { };
+
+        public CustomControl1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+
+            Graphics g = pe.Graphics;
+            Pen pen = new Pen(Color.Black, 2);
+
+            // Horca
+            Pintado(g,pen, 0, Height, Width, Height,false);
+            Pintado(g, pen, 0, 0, 0, Height,false);
+            Pintado(g, pen, 0, 0, Width / 2, 0,false);
+            Pintado(g, pen, Width / 2, 0, (Width / 2), Height / 6,false);
+            
+            g.DrawLine(pen, 0, Height, Width, Height);
+            g.DrawLine(pen, 0, 0, 0, Height);
+            g.DrawLine(pen, 0, 0, Width / 2, 0);
+            g.DrawLine(pen, Width / 2, 0, (Width / 2), Height / 6);
+            // Cabeza
+            Pintado(g, pen, Width / 3, Height / 6, Width / 3, Height / 6,true);
+
+            g.DrawEllipse(pen, Width / 3, Height / 6, Width / 3, Height / 6);
+            //Torso
+            Pintado(g, pen, Width / 2, Height - Height / 6, Width / 2, Height / 3,false);
+
+            g.DrawLine(pen, Width / 2, Height - Height / 6, Width / 2, Height / 3);
+            // Brazos
+            Pintado(g, pen, Width / 10, Height - Height / 3, Width / 2, Height / 2,false);
+            Pintado(g, pen, Width / 2, Height / 2, Width - Width / 10, Height - Height / 3,false);
+
+            g.DrawLine(pen, Width / 10, Height - Height / 3, Width / 2, Height / 2);
+            g.DrawLine(pen, Width / 2, Height / 2, Width - Width / 10, Height -Height / 3);
+            //Piernas
+            Pintado(g, pen, Width / 2, Height - Height / 6, Width / 10, Height - Height / 10,false);
+            Pintado(g, pen, Width / 2, Height - Height / 6, Width - Width / 10, Height - Height / 10,false);
+
+            g.DrawLine(pen, Width / 2, Height - Height / 6, Width / 10, Height - Height / 10);
+            g.DrawLine(pen, Width / 2, Height - Height / 6, Width - Width / 10, Height - Height / 10);
+        }
+    }
+}
---
+++ BoletinJunio/DibujoAhorcado/Ejercicio9/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace Ejercicio9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.customControl12 = new DibujoAhorcado.CustomControl1();
+            this.customControl11 = new DibujoAhorcado.CustomControl1();
+            this.SuspendLayout();
+            // 
+            // customControl12
+            // 
+            this.customControl12.Location = new System.Drawing.Point(132, 67);
+            this.customControl12.Name = "customControl12";
+            this.customControl12.Size = new System.Drawing.Size(75, 240);
+            this.customControl12.TabIndex = 1;
+            this.customControl12.Text = "customControl12";
+            // 
+            // customControl11
+            // 
+            this.customControl11.Location = new System.Drawing.Point(303, 67);
+            this.customControl11.Name = "customControl11";
+            this.customControl11.Size = new System.Drawing.Size(75, 183);
+            this.customControl11.TabIndex = 0;
+            this.customControl11.Text = "customControl11";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.customControl12);
+            this.Controls.Add(this.customControl11);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Ejercicio 9";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DibujoAhorcado.CustomControl1 customControl11;
+        private DibujoAhorcado.CustomControl1 customControl12;
+    }
+}
+
