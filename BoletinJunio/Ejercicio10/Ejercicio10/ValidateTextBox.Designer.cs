10849afc1bfccb0fe266c940472dcfe1b1556a91
Oscar fariña <61348449+ofaricamp@users.noreply.github.com>
2022-05-25 11:19:54 +0200

    Add files via upload

---
+++ BoletinJunio/Ejercicio10/Ejercicio10/ValidateTextBox.Designer.cs
@@ -0,0 +1,61 @@
+﻿
+namespace Ejercicio10
+{
+    partial class ValidateTextBox
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txt
+            // 
+            this.txt.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt.Location = new System.Drawing.Point(17, 34);
+            this.txt.Name = "txt";
+            this.txt.Size = new System.Drawing.Size(100, 20);
+            this.txt.TabIndex = 0;
+            this.txt.TextChanged += new System.EventHandler(this.txt_TextChanged);
+            // 
+            // ValidateTextBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.txt);
+            this.Name = "ValidateTextBox";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt;
+    }
+}
---
+++ BoletinJunio/Ejercicio10/FormEjercicio10/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace FormEjercicio10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textuallbl = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.validateTextBox2 = new Ejercicio10.ValidateTextBox();
+            this.validateTextBox1 = new Ejercicio10.ValidateTextBox();
+            this.SuspendLayout();
+            // 
+            // textuallbl
+            // 
+            this.textuallbl.AutoSize = true;
+            this.textuallbl.Location = new System.Drawing.Point(25, 66);
+            this.textuallbl.Name = "textuallbl";
+            this.textuallbl.Size = new System.Drawing.Size(84, 13);
+            this.textuallbl.TabIndex = 1;
+            this.textuallbl.Text = "TIPO TEXTUAL";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 226);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "TIPO NUMERICO";
+            // 
+            // validateTextBox2
+            // 
+            this.validateTextBox2.Location = new System.Drawing.Point(12, 245);
+            this.validateTextBox2.Name = "validateTextBox2";
+            this.validateTextBox2.Size = new System.Drawing.Size(154, 123);
+            this.validateTextBox2.TabIndex = 2;
+            this.validateTextBox2.TexTxt = "";
+            this.validateTextBox2.Tipo = Ejercicio10.eTipo.Numerico;
+            // 
+            // validateTextBox1
+            // 
+            this.validateTextBox1.Location = new System.Drawing.Point(12, 85);
+            this.validateTextBox1.Name = "validateTextBox1";
+            this.validateTextBox1.Size = new System.Drawing.Size(350, 43);
+            this.validateTextBox1.TabIndex = 0;
+            this.validateTextBox1.TexTxt = "";
+            this.validateTextBox1.Tipo = Ejercicio10.eTipo.Textual;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.validateTextBox2);
+            this.Controls.Add(this.textuallbl);
+            this.Controls.Add(this.validateTextBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Ejercicio10.ValidateTextBox validateTextBox1;
+        private System.Windows.Forms.Label textuallbl;
+        private Ejercicio10.ValidateTextBox validateTextBox2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
